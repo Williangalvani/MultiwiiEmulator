04b911a0f9fd245ea07be5d5b9686ecd11f2a572
Patrick <patrickelectric@gmail.com>
2014-08-09 19:08:50 -0300

    resolvendo erro: http://stackoverflow.com/questions/2011160/unexpected-operator-error

--- socat.sh
+++ socat.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 uid=`id -u $USERNAME`
-if [ "$USER" == "root" ]
+if [ "$USER" = "root" ]
 then
-USER2=$SUDO_USER
-socat PTY,link=/dev/ttyVirtual1 PTY,link=/dev/ttyVirtual2&
-sleep 1
-chown $USER2:dialout /dev/ttyVirtual1
-chown $USER2:dialout /dev/ttyVirtual2
-echo "Created ports, set ownership to user $USER2"
-echo "Connect to /dev/ttyVirtual1 and /dev/ttyVirtual2"
+	USER2=$SUDO_USER
+	socat PTY,link=/dev/ttyVirtual1 PTY,link=/dev/ttyVirtual2&
+	sleep 1
+	chown $USER2:dialout /dev/ttyVirtual1
+	chown $USER2:dialout /dev/ttyVirtual2
+	echo "Created ports, set ownership to user $USER2"
+	echo "Connect to /dev/ttyVirtual1 and /dev/ttyVirtual2"
 else
-echo "RUN WITH SUDO!"
+	echo "RUN WITH SUDO!"
 fi
